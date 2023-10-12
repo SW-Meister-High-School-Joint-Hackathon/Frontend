905068c4dfe5be03fd74cfbe4a1f8d3dbd0dca68
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2023-10-13 04:47:45 +0900

    FIX : CSS 퍼블리싱 수정

--- src/page/Final/index.jsx
+++ src/page/Final/index.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Banner from '../../assets/Banner.png';
 
 const Final = () => {
   const blueK = [2, 8, 7, 0, 2];
@@ -12,15 +13,9 @@ const Final = () => {
   const blueA = [8, 10, 10, 13, 11];
   const redA = [3, 4, 5, 5, 7];
 
-<<<<<<< HEAD
-  const blueA = [8,10,10,13,11];
-  const redA = [3,4,5,5,7];
+  const blueM = [252, 182, 219, 264, 30];
+  const redM = [201, 164, 243, 234, 27];
 
-  const blueM = [252,182,219,264,30];
-  const redM = [201,164,243,234,27];
-  
-=======
->>>>>>> 9e6a2934ececa1b1b025618dd511edce9c9dcbaa
   const [blueTeamData, setBlueTeamData] = useState([]);
   const [redTeamData, setRedTeamData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -48,90 +43,97 @@ const Final = () => {
 
   return (
     <Body>
-      <form>
-        <Table>
-          <tr>
-            <th>Team</th>
-            <th>Player</th>
-            <th>Kill</th>
-            <th>Death</th>
-            <th>Assistance</th>
-            <th>Damage</th>
-            <th>Attack</th>
-            <th>Minion Kill</th>
-            <th>CurrentGold</th>
-            <th>TotalGold</th>
-          </tr>
-          {blueTeamData.map((data, index) => (
+      <div>
+        <img
+          src={Banner}
+          alt="error"
+          style={{ width: '100.5%', marginLeft: '-4px', marginTop: '-25px' }}
+        />
+        <form>
+          <Table>
             <tr>
-              <td style={{ background: '#227BE2' }}>blue</td>
-              <td>
-                {data.personId == 1
-                  ? 'Kiin'
-                  : data.personId == 2
-                  ? 'Cuzz'
-                  : data.personId == 3
-                  ? 'Bdd'
-                  : data.personId == 4
-                  ? 'Aiming'
-                  : data.personId == 5
-                  ? 'Lehends'
-                  : data.personId == 6
-                  ? 'Clear'
-                  : data.personId == 7
-                  ? 'Willer'
-                  : data.personId == 8
-                  ? 'Clozer'
-                  : data.personId == 9
-                  ? 'Teddy'
-                  : 'Kael'}
-              </td>
-              <td>{blueK[index]}</td>
-              <td>{blueD[index]}</td>
-              <td>{blueA[index]}</td>
-              <td>{data.totalDamageDone}</td>
-              <td>{data.totalDamageTaken}</td>
-              <td>{redM[index]}</td>
-              <td>{data.currentGold}</td>
-              <td>{data.totalGold}</td>
+              <th>Team</th>
+              <th>Player</th>
+              <th>Kill</th>
+              <th>Death</th>
+              <th>Assistance</th>
+              <th>Damage</th>
+              <th>Attack</th>
+              <th>Minion Kill</th>
+              <th>CurrentGold</th>
+              <th>TotalGold</th>
             </tr>
-          ))}
-          {redTeamData.map((data, index) => (
-            <tr>
-              <td style={{ background: '#F50564' }}>red</td>
-              <td>
-                {data.personId == 1
-                  ? 'Kiin'
-                  : data.personId == 2
-                  ? 'Cuzz'
-                  : data.personId == 3
-                  ? 'Bdd'
-                  : data.personId == 4
-                  ? 'Aiming'
-                  : data.personId == 5
-                  ? 'Lehends'
-                  : data.personId == 6
-                  ? 'Clear'
-                  : data.personId == 7
-                  ? 'Willer'
-                  : data.personId == 8
-                  ? 'Clozer'
-                  : data.personId == 9
-                  ? 'Teddy'
-                  : 'Kael'}
-              </td>
-              <td>{redK[index]}</td>
-              <td>{blueD[index]}</td>
-              <td>{blueA[index]}</td>
-              <td>{data.totalDamageDone}</td>
-              <td>{data.totalDamageTaken}</td>
-              <td>{blueM[index]}</td>
-              <td>{data.currentGold}</td>
-              <td>{data.totalGold}</td>
-            </tr>
-          ))}
-        </Table>
-      </form>
+            {blueTeamData.map((data, index) => (
+              <tr>
+                <td style={{ background: '#227BE2' }}>blue</td>
+                <td>
+                  {data.personId == 1
+                    ? 'Kiin'
+                    : data.personId == 2
+                    ? 'Cuzz'
+                    : data.personId == 3
+                    ? 'Bdd'
+                    : data.personId == 4
+                    ? 'Aiming'
+                    : data.personId == 5
+                    ? 'Lehends'
+                    : data.personId == 6
+                    ? 'Clear'
+                    : data.personId == 7
+                    ? 'Willer'
+                    : data.personId == 8
+                    ? 'Clozer'
+                    : data.personId == 9
+                    ? 'Teddy'
+                    : 'Kael'}
+                </td>
+                <td>{blueK[index]}</td>
+                <td>{blueD[index]}</td>
+                <td>{blueA[index]}</td>
+                <td>{data.totalDamageDone}</td>
+                <td>{data.totalDamageTaken}</td>
+                <td>{redM[index]}</td>
+                <td>{data.currentGold}</td>
+                <td>{data.totalGold}</td>
+              </tr>
+            ))}
+            {redTeamData.map((data, index) => (
+              <tr>
+                <td style={{ background: '#F50564' }}>red</td>
+                <td>
+                  {data.personId == 1
+                    ? 'Kiin'
+                    : data.personId == 2
+                    ? 'Cuzz'
+                    : data.personId == 3
+                    ? 'Bdd'
+                    : data.personId == 4
+                    ? 'Aiming'
+                    : data.personId == 5
+                    ? 'Lehends'
+                    : data.personId == 6
+                    ? 'Clear'
+                    : data.personId == 7
+                    ? 'Willer'
+                    : data.personId == 8
+                    ? 'Clozer'
+                    : data.personId == 9
+                    ? 'Teddy'
+                    : 'Kael'}
+                </td>
+                <td>{redK[index]}</td>
+                <td>{blueD[index]}</td>
+                <td>{blueA[index]}</td>
+                <td>{data.totalDamageDone}</td>
+                <td>{data.totalDamageTaken}</td>
+                <td>{blueM[index]}</td>
+                <td>{data.currentGold}</td>
+                <td>{data.totalGold}</td>
+              </tr>
+            ))}
+          </Table>
+        </form>
+      </div>
     </Body>
   );
 };
@@ -159,6 +161,7 @@ const Table = styled.table`
   font-weight: 600;
   line-height: normal;
   letter-spacing: 0.88px;
+  margin-top: -12px;
 
   th,
   td {
